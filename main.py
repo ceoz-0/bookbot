cd86f197c63c7913284ebfafcdeb436ba4c83024
CEOZMaze <carlos.ceoz.oliveira@proton.me>
2025-03-17 19:41:01 -0300

    CH2-L6 Count Characters feature implemented :D

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_num_words
-
+from stats import count_chars
 def get_book_text(path):
     with open(path) as f:
         content = f.read()
@@ -10,7 +10,8 @@ def main():
     book_string = get_book_text("./books/frankenstein.txt")
     num_words = get_num_words(book_string)
     print(f"{num_words} words found in the document")
-
+    char_dict = count_chars(book_string)
+    print(char_dict)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@
 def get_num_words(text):
     num_words = text.split()
     return len(num_words)
+
+
+def count_chars(text):
+    char_dict = {}
+    char_count = 0
+    text = text.lower()
+    for char in text:
+        if char not in char_dict:
+            char_dict[char] = 0
+        char_dict[char] +=1
+    return char_dict
